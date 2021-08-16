a782de2ce64fd2ae7432a1ea554d0ba7f8f6faef
jgrynczewski <Tel^)%322))$>
2021-08-16 23:42:41 +0200

    Trasy zalogowanego użytkownika

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 
-from .models import Route, User
+from .models import Route#, User
 
 admin.site.register(Route)
-admin.site.register(User)
+# admin.site.register(User)
--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from users.views import register
+from homepage.models import Route
 
 
+# jeżeli użytkownik jest niezalogowany przekierowujemy na stronę logowania
+@login_required()
 def home(request):
-    if request.user.is_authenticated:
-        return redirect('/users/profile.html') # nie mam pojecia jak zrobic to inaczej
-    else:
-        return register(request)
+
+    # wyciągamy usera
+    user = request.user
+
+    # Z bazy wyciągamy wszystkie trasy przypisane do danego użytkownika
+    routes = Route.objects.filter(user=user)
+
+    return render(
+        request,
+        'homepage/routes.html',
+        context={
+            'routes': routes  # trasy przekazujemy w kontekście do szablony (a w szablonie iterujemy się po nich)
+        }
+    )
+
+    # if request.user.is_authenticated:
+    #     return redirect('/users/profile.html') # nie mam pojecia jak zrobic to inaczej
+    # else:
+    #     return register(request)
 
 
 @login_required
